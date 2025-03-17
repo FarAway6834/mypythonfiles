0bb7630aa0be26ab46c804bbcb4e21153b29c6b6
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-18 08:40:25 +0900

    .......wha...

--- mytempdir.py
+++ mytempdir.py
@@ -1,5 +1,5 @@
 from tempfile import TemporaryDirectory as __tempdir__
-from os.path import getcwd as __pwd__
+from os import getcwd as __pwd__
 from contextlib import contextmanager as __withibler__
 
 class __tempdirman__:
