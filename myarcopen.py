78a9704fb9263f97247c00efb63b86c328c126a0
c0dk1ddy <tax0787bz3@gmail.com>
2025-03-18 12:48:53 +0900

    Update myarcopen.py

--- myarcopen.py
+++ myarcopen.py
@@ -2,8 +2,15 @@ from zipfile import ZipFile as zip
 from tarfile import open as tar
 
 def gentz(s):
-  def tz(file, mode = f'r:{s}z', wv = f'w:{s}z', v = 'w'):
-	  if mode == v: mode = wv
-	  return tar(file, mode)
-  tz.__name__ = f't{s}z'
-  return tz
+	def tz(file, mode = 'r', wv = f'w:{s}z', rv = f'r:{s}z', r = 'r', w = 'w'):
+		match mode:
+			case r: mode = rv;
+			case w: mode = wv;
+			case _: pass
+		return tar(file, mode)
+	tz.__name__ = f't{s}z'
+	return tz
+
+txz, tgz = map(gentz, 'xg')
+
+def open(f, mode = 'r'):
