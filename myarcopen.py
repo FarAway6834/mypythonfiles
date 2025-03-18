922145e8a407046e17e91ef89909210ccdf2fad8
c0dk1ddy <tax0787bz3@gmail.com>
2025-03-18 13:24:35 +0900

    Update myarcopen.py

--- myarcopen.py
+++ myarcopen.py
@@ -1,6 +1,8 @@
 from zipfile import ZipFile as zip
 from tarfile import open as tar
 
+o = open
+
 def gentz(s):
 	def tz(file, mode = 'r', wv = f'w:{s}z', rv = f'r:{s}z', r = 'r', w = 'w'):
 		match mode:
@@ -14,3 +16,9 @@ def gentz(s):
 txz, tgz = map(gentz, 'xg')
 
 def open(f, mode = 'r'):
+	file = (lambda x, y : (x == 'tar', y))(*f.split('.')[-2:])
+	match file:
+		case (True, 'gz') | (False, 'tgz') | (True, 'tgz'): return tgz(f, mode = mode)
+		case (True, 'xz') | (False, 'txz') | (True, 'txz'): return txz(f, mode = mode)
+		case (True, 'whl') | (False, 'whl') | (True, 'zip') | (False, 'zip'): return zip(f, mode = mode)
+		case _: return o(f)
