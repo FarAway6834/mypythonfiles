fe04cf620318f0eb3bb8ae53c8bb518ee58765ed
c0dk1ddy <tax0787bz3@gmail.com>
2025-03-18 12:45:39 +0900

    oh wait

--- myarcopen.py
+++ myarcopen.py
@@ -1,4 +1,9 @@
 from zipfile import ZipFile as zip
 from tarfile import open as tar
 
-def tgz(file, mode = 'r:gz'):
+def gentz(s):
+  def tz(file, mode = f'r:{s}z', wv = f'w:{s}z', v = 'w'):
+	  if mode == v: mode = wv
+	  return tar(file, mode)
+  tz.__name__ = f't{s}z'
+  return tz
